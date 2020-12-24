6acf6c008b3e370031446bbd7453c4f06e2c1503
novalution <57666913+novalution@users.noreply.github.com>
2020-12-24 22:03:39 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,33 +9,37 @@ const iPhoneX = devices["iPhone X"];
 });
   const context = await browser.createIncognitoBrowserContext();  
   const page = await context.newPage();
-  const ali = `https://a.aliexpress.com/_mNypvIZ`;
+  const page2 = await context.newPage();
+  const ali = `https://a.aliexpress.com/_m0Luatx`;
   await page.emulate(iPhoneX);
-  await page.goto(`${ali}`);
-  //await page.waitForSelector('#am-modal-container-1608799656580 > div > div.am-modal-wrap.projectGlobal_ModalHasClose_FlexInTop > div > div > div > div > div:nth-child(1) > div > div > div:nth-child(5) > div.goBtn___3aVca')
-  await page.click('#am-modal-container-1608799656580 > div > div.am-modal-wrap.projectGlobal_ModalHasClose_FlexInTop > div > div > div > div > div:nth-child(1) > div > div > div:nth-child(5) > div.goBtn___3aVca')
-  await page.waitForSelector('#root > ul > li:nth-child(1)')
-  await page.click('#root > ul > li:nth-child(1)')
-  await page.type('#root > div.fm-tabs-content > div:nth-child(1) > div > div > div:nth-child(2) > input','novalreff10@live.com')
-  await page.type('#root > div.fm-tabs-content > div:nth-child(1) > div > div > div:nth-child(3) > input','lalalili')
-  await page.waitForSelector('#root > div.fm-tabs-content > div:nth-child(1) > div > div > button')
-  await page.click('#root > div.fm-tabs-content > div:nth-child(1) > div > div > button')
-  await page.goto('https://aliexpress.com')
-  await page.waitForSelector('#footer-account > svg')
-  await page.click('#footer-account > svg')
-  await page.waitForSelector('##account-section > div > div > div:nth-child(4) > ul:nth-child(2) > li:nth-child(1)')
-  await page.click('#account-section > div > div > div:nth-child(4) > ul:nth-child(2) > li:nth-child(1)')
-  await page.waitForSelector('#account-section > div > div > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1)')
-  await page.click('#account-section > div > div > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1)')
-  await page.waitForSelector('#account-section > div > div > div.swipeable-views > div > div > ul > div:nth-child(2) > span > span:nth-child(2)')
-  await page.click('#account-section > div > div > div.swipeable-views > div > div > ul > div:nth-child(2) > span > span:nth-child(2)')
-  await page.waitForSelector('#account-section > div > div > div.swipeable-views.with-survey > div > div:nth-child(2) > ul > div:nth-child(4)')
-  await page.click('#account-section > div > div > div.swipeable-views.with-survey > div > div:nth-child(2) > ul > div:nth-child(4)')
-  await page.waitForSelector('#account-section > div > div > div.swipeable-views.with-survey > div > div:nth-child(3) > ul > div:nth-child(6)')
-  await page.click('#account-section > div > div > div.swipeable-views.with-survey > div > div:nth-child(3) > ul > div:nth-child(6)')
-  await page.goto(`${ali}`)
-  await page.waitForSelector('#am-modal-container-1608800620474 > div > div.am-modal-wrap.projectGlobal_ModalHasClose_FlexInTop > div > div > div > div > div:nth-child(1) > div > div > div:nth-child(5) > div.goBtn___3aVca')
-  await page.click('#am-modal-container-1608800620474 > div > div.am-modal-wrap.projectGlobal_ModalHasClose_FlexInTop > div > div > div > div > div:nth-child(1) > div > div > div:nth-child(5) > div.goBtn___3aVca')
+  await page.goto(`${ali}`,{delay:1000});
+  // await page.waitForSelector('#am-modal-container-1608799656580 > div > div.am-modal-wrap.projectGlobal_ModalHasClose_FlexInTop > div > div > div > div > div:nth-child(1) > div > div > div:nth-child(5) > div.goBtn___3aVca',{delay:1000})
+  // await page.click('#am-modal-container-1608799656580 > div > div.am-modal-wrap.projectGlobal_ModalHasClose_FlexInTop > div > div > div > div > div:nth-child(1) > div > div > div:nth-child(5) > div.goBtn___3aVca',{delay:1000})
+  await page.waitForSelector('#root > ul > li:nth-child(1)',{delay:1000})
+  await page.click('#root > ul > li:nth-child(1)',{delay:1000})
+  await page.click('#root > div.fm-tabs-content > div:nth-child(1) > div > div > div:nth-child(2) > input',{delay:1000})
+  await page.type('#root > div.fm-tabs-content > div:nth-child(1) > div > div > div:nth-child(2) > input','novalreff126@live.com',{delay:300})
+  await page.type('#root > div.fm-tabs-content > div:nth-child(1) > div > div > div:nth-child(3) > input','lalalili',{delay:300})
+  await page.waitForSelector('#root > div.fm-tabs-content > div:nth-child(1) > div > div > button',{delay:1000})
+  await page.click('#root > div.fm-tabs-content > div:nth-child(1) > div > div > button',{delay:1000})
+  
+  await page2.emulate(iPhoneX);
+  await page2.goto('https://aliexpress.com',{delay:30000})
+  await page2.waitForSelector('#footer-account > svg',{delay:1000})
+  await page2.click('#footer-account > svg',{delay:1000})
+  await page2.waitForSelector('#account-section > div > div > div:nth-child(4) > ul:nth-child(2) > li:nth-child(1)'),{delay:1000}
+  await page2.click('#account-section > div > div > div:nth-child(4) > ul:nth-child(2) > li:nth-child(1)',{delay:1000})
+  await page2.waitForSelector('#account-section > div > div > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1)',{delay:1000})
+  await page2.click('#account-section > div > div > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1)',{delay:1000})
+  await page2.waitForSelector('#account-section > div > div > div.swipeable-views > div > div > ul > div:nth-child(2) > span > span:nth-child(2)',{delay:1000})
+  await page2.click('#account-section > div > div > div.swipeable-views > div > div > ul > div:nth-child(2) > span > span:nth-child(2)',{delay:1000})
+  await page2.waitForSelector('#account-section > div > div > div.swipeable-views.with-survey > div > div:nth-child(2) > ul > div:nth-child(4)',{delay:1000})
+  await page2.click('#account-section > div > div > div.swipeable-views.with-survey > div > div:nth-child(2) > ul > div:nth-child(4)',{delay:1000})
+  await page2.waitForSelector('#account-section > div > div > div.swipeable-views.with-survey > div > div:nth-child(3) > ul > div:nth-child(6)',{delay:1000})
+  await page2.click('#account-section > div > div > div.swipeable-views.with-survey > div > div:nth-child(3) > ul > div:nth-child(6)',{delay:1000})
+  await page.reload({delay:1000})
+  // await page.waitForSelector('#am-modal-container-1608800620474 > div > div.am-modal-wrap.projectGlobal_ModalHasClose_FlexInTop > div > div > div > div > div:nth-child(1) > div > div > div:nth-child(5) > div.goBtn___3aVca')
+  // await page.click('#am-modal-container-1608800620474 > div > div.am-modal-wrap.projectGlobal_ModalHasClose_FlexInTop > div > div > div > div > div:nth-child(1) > div > div > div:nth-child(5) > div.goBtn___3aVca')
   await page.waitForSelector('#am-modal-container-1608800678816 > div > div.am-modal-wrap > div > div > div > div > div.innerContentWrapper___24iXr > div.modalInnerContent___6bRf9 > div > div.box___3P30A > h1')
   await page.click('#am-modal-container-1608800678816 > div > div.am-modal-wrap > div > div > div > div > div.innerContentWrapper___24iXr > div.modalInnerContent___6bRf9 > div > div.box___3P30A > h1')
   await page.waitForSelector('#copylink')
